8d3075fedd456540940444c84ca877cb5ee4bf5d
sonyavera <sonyavgould@gmail.com>
2020-10-11 10:08:57 -0400

    new checkout component

---
+++ src/components/CheckoutForm.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import {CardElement, injectStripe} from 'react-stripe-elements';
+
+class CheckoutForm extends Component {
+
+
+    state = {
+        complete: false
+    }
+
+    submit = async () => {
+        let {token} = await this.props.stripe.createToken({name: 'Name'});
+        let response = await fetch('http://localhost:3000/charges', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({
+                token: token.id,
+                orderId: this.props.orderId
+            })
+        })
+
+        if(response.ok){
+            this.setState({
+                complete: true
+            })
+        }
+    }
+
+    render(){
+
+        if(this.state.complete) return <h1>Purchase complete!</h1>
+
+        return(
+            <div className="checkout">
+                <p>Would you like to complete the purchase?</p>
+                <CardElement/>
+                <button onClick={this.submit}>Purchase</button>
+            </div>
+        )
+    }
+
+}
+
+export default injectStripe(CheckoutForm);
